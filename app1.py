58c045c6fac62c128e571a6fa5844d59cd4ad8fb
Pavan Katakam <127151808+Pavan125@users.noreply.github.com>
2025-01-29 16:11:54 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -182,7 +182,7 @@ selected_languages = st.multiselect("Select languages for translation:", list(la
 
 if youtube_link:
     video_id = youtube_link.split("v=")[1]
-    st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
+    st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_container_width=True)
 
 if st.button("Get Summarized Notes"):
     transcript_text = extract_transcript_details(youtube_link)
